1190c946eb159785b2cfab7556a2e7ae78e365af
inDream <2394866+inDream@users.noreply.github.com>
2020-08-14 16:18:18 +0800

    Add zbar wasm

---
+++ zbar/precache-manifest.fc1dc9872c9a52a2ac498e143b14190f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0827b410f4506175239b3a3550411cfaaf2c5b41",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "a46adcea5ada856e94f1",
+    "url": "/demo/static/js/main.4a79c4c0.chunk.js"
+  },
+  {
+    "revision": "291c6bec69179f1e0dca",
+    "url": "/demo/static/js/runtime-main.134dfdfb.js"
+  },
+  {
+    "revision": "9b11a5a91b79d73997066c5fa9ba8a475fe18cbc",
+    "url": "/demo/static/media/zbar.wasm.1476250b.bin"
+  }
+]);
